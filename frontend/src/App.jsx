94d1d409c51f06344db6f6bb85aa200e9b788747
AstolphoW <wagner11.serra@gmail.com>
2025-04-29 19:32:00 -0300

    Conectando frontend ao backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,14 @@
+import { useEffect, useState } from 'react'
+
 export default function App() {
-  return <h1>Bem-vindo ao Manasys (Frontend)</h1>
+  const [mensagem, setMensagem] = useState('Carregando...')
+
+  useEffect(() => {
+    fetch('https://manasys-backend.onrender.com/')
+      .then(res => res.json())
+      .then(data => setMensagem(data.message))
+      .catch(err => setMensagem('Erro ao conectar com o backend'))
+  }, [])
+
+  return <h1>{mensagem}</h1>
 }
